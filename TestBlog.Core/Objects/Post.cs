27a41abe29589b5055de179d6170664b857b2260
Gareth Owen <gareth.w.owen@gmail.com>
2015-09-16 21:58:53 +0100

    Added category, tag and post classes

---
+++ TestBlog.Core/Objects/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBlog.Core.Objects
+{
+    public class Post
+    {
+        public virtual int Id
+        { get; set; }
+
+        public virtual string Title
+        { get; set; }
+
+        public virtual string ShortDescription
+        { get; set; }
+
+        public virtual string Description
+        { get; set; }
+
+        public virtual string Meta
+        { get; set; }
+
+        //for my own editification:
+        //URL Slug
+        //A URL slug is a SEO- and user-friendly string-part in a URL to identify, 
+        //describe and access a resource. Often the title of a page/article is a valid candidate.
+
+        public virtual string UrlSlug
+        { get; set; }
+
+        public virtual bool Published
+        { get; set; }
+
+        public virtual DateTime PostedOn
+        { get; set; }
+
+        public virtual DateTime? Modified
+        { get; set; }
+
+        public virtual Category Category
+        { get; set; }
+
+        public virtual IList<Tag> Tag
+        { get; set; }
+    }
+}
