815b4f55ab2cd0a908f7017689044d1f6a425bd1
Gareth Owen <gareth.w.owen@gmail.com>
2015-09-17 20:19:24 +0100

    Added mappings, repository classes and blog controller to TestBlog

---
+++ TestBlog/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestBlog.Core;
+
+namespace TestBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+       public class BlogController
+       {
+           private readonly IBlogRepository _blogRepository;
+
+           public BlogController(IBlogRepository blogRepository)
+           {
+               _blogRepository = blogRepository;
+           }
+
+           public ViewResult Posts(int p = 1)
+           {
+               //TODO: read and return posts from repository
+           }
+       }
+
+    }
+}
