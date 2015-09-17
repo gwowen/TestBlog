8af10cd2d96645264e303433db67efec9eeb405c
Gareth Owen <gareth.w.owen@gmail.com>
2015-09-17 20:47:47 +0100

    Corrected dumb error in Posts action in BlogController, added ListViewModel

--- TestBlog/Controllers/BlogController.cs
+++ TestBlog/Controllers/BlogController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TestBlog.Core;
+using TestBlog.Core.Objects;
+using TestBlog.Models;
 
 namespace TestBlog.Controllers
 {
     public class BlogController : Controller
     {
-       public class BlogController
-       {
            private readonly IBlogRepository _blogRepository;
 
            public BlogController(IBlogRepository blogRepository)
@@ -20,7 +20,10 @@ namespace TestBlog.Controllers
 
            public ViewResult Posts(int p = 1)
            {
-               //TODO: read and return posts from repository
+               var viewModel = new ListViewModel(_blogRepository, p);
+
+               ViewBag.Title = "Latest Posts";
+               return View("List", viewModel);
            }
        }
 
