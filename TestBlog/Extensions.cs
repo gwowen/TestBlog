e8a03a551493648c691f57d65dedbc359739f06a
Gareth Owen <gareth.w.owen@gmail.com>
2015-09-20 18:19:58 +0100

    Added extensions class, actionlinkextensions class, completed List.cshtml and added TestBlog to Web.config

---
+++ TestBlog/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TestBlog.Core.Objects;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace TestBlog
+{
+    public static class Extensions
+    {
+        public static string ToConfigLocalTime(this DateTime utcDt)
+        {
+            ///<summary>
+            ///Convert the passed DateTime from UTC timezone to configured timezone in web.config
+            ///</summary>
+            /// <param name="utcDT"></param>
+            /// <returns></returns>
+            var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+            return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDt, istTZ).ToShortDateString(), 
+                ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        }
+    }
+}
